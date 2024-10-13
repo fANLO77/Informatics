345664de255a1904562b0716ea9207cce1542e9e
fANLO77 <20060803d@gmail.com>
2024-10-13 23:34:56 +0700

    Add files via upload

---
+++ laba5/2.py
@@ -0,0 +1,3 @@
+a = int(input("введи число a: "))
+k = int(input("введи число k: "))
+print(a%(10**k))
---
+++ laba5/3.py
@@ -0,0 +1,5 @@
+a = int(input("Введите число a: "))
+sot = a//100
+des = (a%100)//10
+ed = a%10
+print(sot + des + ed)
